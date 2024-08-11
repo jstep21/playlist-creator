97ac27533f98b38b889c5e90ccea81d54a60ee79
jstep21 <jstep21@github.com>
2024-08-11 23:41:45 +0200

    removed cache path causing internal server error

--- main.py
+++ main.py
@@ -151,18 +151,11 @@ def home():
 
 
 def create_spotify_oauth():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    cache_path = os.path.join(base_dir, '.cache')
-
-    if not os.path.exists(cache_path):
-        os.makedirs(cache_path)
-
     return SpotifyOAuth(
         client_id=SPOTIPY_CLIENT_ID,
         client_secret=SPOTIPY_CLIENT_SECRET,
         redirect_uri=SPOTIPY_REDIRECT_URI,
-        scope=SCOPE,
-        cache_path=cache_path
+        scope=SCOPE
     )
 
 
